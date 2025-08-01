5ed4130ce017df96d36e44bc605265ffb6383938
ahmed-abd-alalim <ahmedabdalalim.3a@gmail.com>
2025-08-02 00:51:51 +0300

    import all icons

---
+++ src/assets/icons.js
@@ -0,0 +1,4 @@
+import { RiDiscordFill, RiInstagramFill, RiTwitterXFill } from "react-icons/ri";
+import { HiMiniArrowUpRight } from "react-icons/hi2";
+
+export { RiDiscordFill, RiInstagramFill, RiTwitterXFill, HiMiniArrowUpRight };
